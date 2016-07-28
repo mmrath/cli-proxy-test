b3cdc23c3d3f08a71c3061a3560ccae8ee26e14d
Murali Mohan Rath <mmrath@gmail.com>
2016-07-28 21:59:45 +1000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  usersUrl = '/users';  // URL to web api
+
+  users = null;
+
+  constructor(private http: Http) {
+    this.http.get(this.usersUrl)
+      .subscribe(response => {
+          console.log("Received data" + response.json());
+          this.users = response.json();
+        },
+        err=> {
+          console.log("Error getting data:"+ err);
+        });
+  }
+
+
 }
